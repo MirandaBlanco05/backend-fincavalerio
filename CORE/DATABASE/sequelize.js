3a23ce70a5bbfa7b2d213fd02c137681cce4601a
Daniela01plwqpo <Danielavalerionu@gmail.com>
2026-05-05 16:33:30 -0400

    config database render

--- CORE/DATABASE/sequelize.js
+++ CORE/DATABASE/sequelize.js
@@ -1,23 +1,24 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 const conectarDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("✅ Sequelize conectado correctamente a la BD");
+    console.log("✅ Conectado a PostgreSQL en Render");
   } catch (error) {
-    console.error("❌ Error al conectar Sequelize:", error.message);
+    console.error("❌ Error al conectar:", error.message);
   }
 };
 
